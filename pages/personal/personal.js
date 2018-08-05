5b295f50cd6b26bcf48cb3c21f0efc257c0116d7
zywaiting <1573240324@qq.com>
2018-08-06 02:52:04 +0800

    ...

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,4 +1,8 @@
 // pages/personal/personal.js
+//获取应用实例
+const app = getApp()
+const recorderManager = wx.getRecorderManager()
+const innerAudioContext = wx.createInnerAudioContext()
 Page({
 
   /**
@@ -12,7 +16,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
@@ -71,6 +75,82 @@ Page({
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         var tempFilePaths = res.tempFilePaths
+      }
+    })
+  },
+  //开始录音的时候
+  start: function () {
+    const options = {
+      duration: 10000,//指定录音的时长，单位 ms
+      sampleRate: 16000,//采样率
+      numberOfChannels: 1,//录音通道数
+      encodeBitRate: 96000,//编码码率
+      format: 'mp3',//音频格式，有效值 aac/mp3
+      frameSize: 50,//指定帧大小，单位 KB
+    }
+    //开始录音
+    recorderManager.start(options);
+    recorderManager.onStart(() => {
+      console.log('recorder start')
+    });
+    //错误回调
+    recorderManager.onError((res) => {
+      console.log(res);
+    })
+  },
+  //暂停录音
+  pause: function () {
+    recorderManager.onPause();
+    console.log('暂停录音')
+  },
+  //停止录音
+  stop: function () {
+    recorderManager.stop();
+    recorderManager.onStop((res) => {
+      this.tempFilePath = res.tempFilePath;
+      console.log('停止录音', res.tempFilePath)
+      const { tempFilePath } = res
+    })
+  },
+  //播放声音
+  play: function () {
+    innerAudioContext.autoplay = true
+    innerAudioContext.src = this.tempFilePath,
+      innerAudioContext.onPlay(() => {
+        console.log('开始播放')
+      })
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+
+  },
+  //上传录音
+  upload: function () {
+    wx.uploadFile({
+      url: "https://www.zhuyao.xin/fileUpload",//演示域名、自行配置
+      filePath: this.tempFilePath,
+      name: 'file',
+      header: {
+        "Content-Type": "multipart/form-data"
+      },
+      formData:
+        {
+          userId: 12345678 //附加信息为用户ID
+        },
+      success: function (res) {
+        console.log(res);
+        wx.showToast({
+          title: '上传成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: function (res) {
+        console.log(res);
+      },
+      complete: function (res) {
+
       }
     })
   }
