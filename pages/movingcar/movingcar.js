bc5bae2a92395c3d5b741e0e6ece5a7b72f39859
wq_zy <1573240324@qq.com>
2018-09-03 00:55:27 +0800

    ...

--- pages/movingcar/movingcar.js
+++ pages/movingcar/movingcar.js
@@ -1,25 +1,57 @@
 // pages/movingcar/movingcar.js
+var bmap = require('../../lib/bmap-wx.min.js');
+var wxMarkerData = [];
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    markers: [],
+    latitude: '',
+    longitude: '',
+    rgcData: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    var BMap = new bmap.BMapWX({
+      ak: 'uvluntWrU8gxtNAvTtOVTFhZCMavAypQ'
+    });
+
   
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      wxMarkerData = data.wxMarkerData;
+      that.setData({
+        markers: wxMarkerData
+      });
+      that.setData({
+        latitude: wxMarkerData[0].latitude
+      });
+      that.setData({
+        longitude: wxMarkerData[0].longitude
+      });
+    }
+    BMap.regeocoding({
+      fail: fail,
+      success: success,
+      iconPath: '../../img/marker_red.png',
+      iconTapPath: '../../img/marker_red.png'
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    // 使用 wx.createMapContext 获取 map 上下文
+    this.mapCtx = wx.createMapContext('map')
   },
 
   /**
@@ -67,5 +99,19 @@ Page({
     wx.makePhoneCall({
       phoneNumber: '17605674666' //仅为示例，并非真实的电话号码
     })
-  }
+  },
+  touchMove: function () {
+    var that = this;
+    this.mapCtx.getCenterLocation({
+      success: function (res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
+        that.setData({
+          latitude: res.longitude,
+          longitude: res.latitude
+        })
+      }
+    })
+  },
+
 })
